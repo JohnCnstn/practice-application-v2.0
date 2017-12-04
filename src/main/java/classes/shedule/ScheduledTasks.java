65dfffa28043bee4f5e639b6580d8f6d6b1d8c78
Pavel Khankevich <crystalfaster@gmail.com>
2017-12-05 02:12:30 +0300

    add cron to quartz (now sout date at 0:00 every day)

--- src/main/java/classes/shedule/ScheduledTasks.java
+++ src/main/java/classes/shedule/ScheduledTasks.java
@@ -1,5 +1,9 @@
 package classes.shedule;
 
+import classes.data.entity.Student;
+import classes.data.repository.StudentRepository;
+import classes.data.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -8,10 +12,22 @@ import java.util.Date;
 
 @Component
 public class ScheduledTasks {
+
+    @Autowired
+    private StudentService studentService;
+
+    @Autowired
+    private StudentRepository studentRepository;
+
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
+    @Scheduled(cron = "0 0 0 * * *", zone="Europe/Minsk")
     public void reportCurrentTime() {
+
+        Student student = studentService.findOne(2);
+        student.setAvgScore(5);
+        studentRepository.save(student);
+
         System.out.println("The time is now " + dateFormat.format(new Date()));
     }
 }
