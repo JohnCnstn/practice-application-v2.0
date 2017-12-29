8e210d942c4a68fec9ee09126da3bf3792923e33
Pavel Khankevich <crystalfaster@gmail.com>
2017-12-29 04:49:40 +0300

    fixed load universities

--- src/main/webapp/resources/js/student-view/loader/loadUniversities.js
+++ src/main/webapp/resources/js/student-view/loader/loadUniversities.js
@@ -1,5 +1,7 @@
 $( document ).ready(function() {
 
+    var html = '';
+
     // GET REQUEST
     $("#getAllUniversities").click(function(event){
         event.preventDefault();
@@ -12,19 +14,22 @@ $( document ).ready(function() {
             type : "GET",
             url: window.location + "/getAllUniversities",
             success: function (result) {
-                var html = '';
                 var len = result.length;
+
                 if (len > 0) {
                     $(".submitFaculty").prop('disabled', false);
                 } else {
                     callErrorAlert("You should create at least 1 university!");
                     $(".submitFaculty").prop('disabled', true);
                 }
-                for(var i=0; i<len; i++){
-                    html += '<option value="' + result[i].id + '">' + result[i].name + '</option>';
+
+                if (html.length == 0) {
+                    for(var i=0; i<len; i++){
+                        html += '<option value="' + result[i].id + '">' + result[i].name + '</option>';
+                    }
+                    $('select#facultyId').append(html);
+                    console.log("Success: ", result);
                 }
-                $('select#universityId').append(html);
-                console.log("Success: ", result);
             },
             error : function(e) {
                 console.log("ERROR: ", e);
